467ac10ed064904bf4a76b945637761e7fb47031
TartCodes <tartcodes@gmail.com>
2022-05-10 20:28:54 -0400

    Daily Codewars

---
+++ 051022-Filter/js/main.js
@@ -0,0 +1,7 @@
+
+// .filter Take an array and remove every second element from the array. 
+// Always keep the first element and start removing with the next element.
+
+function removeEveryOther(arr){
+    return arr.filter((e,i) => (i % 2) == 0) 
+  }
