fa6fec28ede1eff812edcaddc9194a111f104d6c
TartCodes <tartcodes@gmail.com>
2022-05-18 13:29:06 -0400

    Daily CW

--- 05-18-22-Arr-plus-Arr/js/main.js
+++ 05-18-22-Arr-plus-Arr/js/main.js
@@ -10,3 +10,9 @@ function arrayPlusArray(arr1,arr2) {
   const sum = newArr.reduce((pv, cv) => pv + cv, value)
   return sum
 }
+
+//or 
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
