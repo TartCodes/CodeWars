b88aeb227de11f4bc6440094c322aa5013f1eb64
TartCodes <tartcodes@gmail.com>
2022-05-20 09:09:54 -0400

    Daily CW

---
+++ 05-20-22-Reverse-pop-shift/js/main.js
@@ -0,0 +1,23 @@
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+// Simples!
+
+function fixTheMeerkat(arr) {
+  return arr.reverse()
+ }
+ 
+ or 
+ 
+ function fixTheMeerkat(arr) {
+  let end = arr.shift()
+  let start = arr.pop()
+ 
+  arr.push(end)
+  arr.unshift(start)
+   
+   return arr
+ }
