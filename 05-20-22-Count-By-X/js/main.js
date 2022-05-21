e959aee9ab5bb69d6fc0ef5758198db9c19de156
TartCodes <tartcodes@gmail.com>
2022-05-21 11:22:35 -0400

    Daily CW

---
+++ 05-20-22-Count-By-X/js/main.js
@@ -0,0 +1,33 @@
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+  let z = [];
+    for(let i = 1; i <= n; i++ ) {
+      if(x % i === 0){
+        z.push(x * i)
+      }
+    }
+    return z;
+}
+
+or 
+
+function countBy(x, n) {
+  let z = [];
+  let limit = n * x
+  for (let i = 1; i <= limit; i++) {
+    if(i % x === 0) {
+      z.push(i)  
+    }
+  }
+
+  return z;
+}
