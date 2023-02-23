e8286650de297521c0d039ecf4e387a37229a804
TartCodes <tartcodes@gmail.com>
2023-02-22 21:26:27 -0500

    random

--- 12-29-22-sort-string-arr/index.js
+++ 12-29-22-sort-string-arr/index.js
@@ -10,7 +10,21 @@
 
 // All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 
-function sortByLength(arr) {
-  // Return an array containing the same strings, ordered from shortest to longest
-  return arr.sort((a, b) => a.length - b.length);
-}
+// function sortByLength(arr) {
+//   // Return an array containing the same strings, ordered from shortest to longest
+//   return arr.sort((a, b) => a.length - b.length);
+// }
+
+let str = "I have no idea what I am GOING TO DO";
+
+const capitalizeEveryOtherLetter = (str) => {
+	let newStr = str.toLowerCase().split(" ").join("").split("");
+	for (let i = 1; i < newStr.length; i++) {
+		if (i % 2 !== 0) {
+			newStr[i] = newStr[i].toUpperCase();
+		}
+	}
+	console.log(newStr.toString().split(",").join(""));
+};
+
+capitalizeEveryOtherLetter(str);
